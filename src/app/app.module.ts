cb90059019d4cda99b9f6e8427b74240d956ba23
Mark Michaels <mmichaels@OnBase.net>
2018-07-13 14:04:41 -0400

    another commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+
 import { CharacterComponent } from './character/character.component';
 
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
 import { PlayerComponent } from './player/player.component';
-import { MessagesComponent } from './messages/messages.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
     CharacterComponent,
-    HeroDetailComponent,
     PlayerComponent,
-    MessagesComponent,
   ],
   imports: [
     BrowserModule,
